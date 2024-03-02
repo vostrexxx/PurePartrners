756b47f37e9f66f0b794f08e2aa03819fd015d59
Vostrikov Nikita <gamilforgpt@gmail.com>
2024-03-02 17:27:40 +0300

    Perfect REG&LOG module

--- PPartners/src/Components/LoginSignup/LoginSignup.jsx
+++ PPartners/src/Components/LoginSignup/LoginSignup.jsx
@@ -5,11 +5,9 @@ import name_icon from "../../assets/name_icon.png";
 import password_icon from "../../assets/password_icon.png";
 import phone_icon from "../../assets/phone_icon.png";
 
-
-
-function InputField({ imageSrc, inputType, placeholder }) {
+function InputField({ imageSrc, inputType, placeholder, isActive }) {
   return (
-    <div className="input">
+    <div className={`input ${isActive ? 'active' : ''}`}>
       <img src={imageSrc} alt="" className="icon" />
       <input type={inputType} placeholder={placeholder} className="input-field" />
     </div>
@@ -17,34 +15,45 @@ function InputField({ imageSrc, inputType, placeholder }) {
 }
 
 function SubmitContainers({ onRegister, onLogin, isRegistering }) {
-    return (
-      <div className="submit-containers">
-        <button
-          className={`submit-container-signup ${isRegistering ? 'active' : ''}`}
-          onClick={onRegister}
-        >
-          Регистрация
-        </button>
-        <button
-          className={`submit-container-login ${!isRegistering ? 'active' : ''}`}
-          onClick={onLogin}
-        >
-          Вход
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="submit-containers">
+      <button
+        className={`submit-container-signup ${isRegistering ? 'active' : 'inactive-signup'}`}
+        onClick={onRegister}
+      >
+        Регистрация
+      </button>
+      <button
+        className={`submit-container-login ${!isRegistering ? 'active' : 'inactive-login'}`}
+        onClick={onLogin}
+      >
+        Вход
+      </button>
+    </div>
+  );
+}
 
 function ForgotPassword() {
+  const handleForgotPasswordClick = () => {
+    // Ваша логика для обработки клика на "Забыли пароль?"
+    console.log("Забыли пароль? кликнули");
+  };
+
   return (
-    <div className="forgotPassword">
-      <span>Забыли пароль?</span>
+    <div className="forgotPassword" onClick={handleForgotPasswordClick}>
+      <span className="forgotPasswordText">Забыли пароль?</span>
     </div>
   );
 }
 
 export default function LoginSignup() {
   const [isRegistering, setIsRegistering] = useState(false);
+  const [activeFields, setActiveFields] = useState({
+    name: false,
+    phone: false,
+    password1: false,
+    password2: false
+  });
 
   const handleRegister = () => {
     setIsRegistering(true);
@@ -63,22 +72,56 @@ export default function LoginSignup() {
 
       {isRegistering && (
         <>
-          <InputField imageSrc={name_icon} inputType="text" placeholder="Введите ФИО" />
-          <InputField imageSrc={phone_icon} inputType="tel" placeholder="Введите номер телефона" />
-          <InputField imageSrc={password_icon} inputType="password" placeholder="Введите пароль" />
-          <InputField imageSrc={password_icon} inputType="password" placeholder="Повторите пароль" />
+          <InputField
+            imageSrc={name_icon}
+            inputType="text"
+            placeholder="Введите ФИО"
+            isActive={activeFields.name}
+          />
+          <InputField
+            imageSrc={phone_icon}
+            inputType="tel"
+            placeholder="Введите номер телефона"
+            isActive={activeFields.phone}
+          />
+          <InputField
+            imageSrc={password_icon}
+            inputType="password"
+            placeholder="Введите пароль"
+            isActive={activeFields.password1}
+          />
+          <InputField
+            imageSrc={password_icon}
+            inputType="password"
+            placeholder="Повторите пароль"
+            isActive={activeFields.password2}
+          />
         </>
       )}
 
       {!isRegistering && (
         <>
-          <InputField imageSrc={phone_icon} inputType="tel" placeholder="Введите номер телефона" />
-          <InputField imageSrc={password_icon} inputType="password" placeholder="Введите пароль" />
+          <InputField
+            imageSrc={phone_icon}
+            inputType="tel"
+            placeholder="Введите номер телефона"
+            isActive={activeFields.phone}
+          />
+          <InputField
+            imageSrc={password_icon}
+            inputType="password"
+            placeholder="Введите пароль"
+            isActive={activeFields.password1}
+          />
         </>
       )}
 
       <ForgotPassword />
-      <SubmitContainers onRegister={handleRegister} onLogin={handleLogin} />
+      <SubmitContainers 
+        onRegister={handleRegister} 
+        onLogin={handleLogin} 
+        isRegistering={isRegistering} 
+      />
     </div>
   );
 }
