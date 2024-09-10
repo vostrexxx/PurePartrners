c96d3716b3e3b386dd69e49355d6f7ec4aab8ad2
Insolent <rrevva@gmail.com>
2024-09-10 15:27:42 +0300

    hotfix

---
+++ customer-info/src/test/java/partners/customer_info/CustomerInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package partners.customer_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
