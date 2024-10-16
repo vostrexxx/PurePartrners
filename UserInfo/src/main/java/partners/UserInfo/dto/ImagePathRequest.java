f00b7c31f04f6309afa493c97c4c3f26e08b0e81
manfeo <olegmuk@mail.ru>
2024-10-16 22:17:10 +0300

    Hotfix for model

--- UserInfo/src/main/java/partners/UserInfo/dto/ImagePathRequest.java
+++ UserInfo/src/main/java/partners/UserInfo/dto/ImagePathRequest.java
@@ -1,10 +1,12 @@
 package partners.UserInfo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ImagePathRequest {
+    @JsonProperty
     private String imagePath;
 }
