ce943f560e102f5b5a4cbae719e6f9d0ee8579ba
Oleg <olegmuk@mail.ru>
2024-10-22 22:38:48 +0300

    small changes to make everything work

--- announcement-info/src/main/java/partners/announcement_info/dto/AnnouncementInfoPreview.java
+++ announcement-info/src/main/java/partners/announcement_info/dto/AnnouncementInfoPreview.java
@@ -2,9 +2,11 @@ package partners.announcement_info.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AnnouncementInfoPreview {
     private Long id;
     private String workCategories;
